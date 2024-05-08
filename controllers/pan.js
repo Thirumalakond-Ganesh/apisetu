e83ca037c03ef35c32e0633487d3b2dcafff2126
amit1208 <amitrajput.chauhan@gmail.com>
2024-05-08 12:23:15 +0530

    test demo

--- controllers/pan.js
+++ controllers/pan.js
@@ -1,5 +1,6 @@
 const { itemId } = require("./itemId");
 const jwt = require("jsonwebtoken");
+const axios=require("axios");
 const fields =[{pan:"95601"},{aadhaar:"95602"},{gst:"95603"},{voterId:"95604"}];
 
 
@@ -7,36 +8,202 @@ exports.panverify = async (req, res) => {
   try {
     const { service, itemId, task, essentials } = req.body;
 
-    if (!service || !itemId || task != "verification" || !essentials) {
-      return res
-        .status(400)
-        .json({ error: "Missing required fields in the request body" });
+    if (!service || !itemId || task !== "verification" || !essentials) {
+      return res.status(400).json({ error: "Missing required fields in the request body" });
     }
 
-    const itemIdVerify = jwt.verify(itemId, process.env.JWT_SECRET)
-      if(itemIdVerify.fldID ==='95601'){
+    const itemIdVerify = jwt.verify(itemId, process.env.JWT_SECRET);
+    if (itemIdVerify.fldID === '95601') {
+      const { pan, fuzzy, panStatus } = essentials;
+      console.log("true fldID");
+      const isValidPAN = (pan) => /^[A-Z]{5}[0-9]{4}[A-Z]$/.test(pan);
+      
+      if (!isValidPAN(pan) || !fuzzy || !panStatus) {
+        console.log(pan);
+        return res.status(400).json({ error: "Missing personal verification fields in the request body" });
+      } else {
+        console.log(pan);
 
-        const { name, pan, fuzzy, panStatus } = essentials;
+        const response = await axios.post('https://sandbox.surepass.io/api/v1/pan/pan', {
+          id_number: pan 
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMzk1MzQ4NiwianRpIjoiNjAwYzk1YzUtMjhkMi00OTBkLWIxOTMtNThkNGM4MzlkOGI5IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2Lm5taXRAc3VyZXBhc3MuaW8iLCJuYmYiOjE3MTM5NTM0ODYsImV4cCI6MTcxNTI0OTQ4NiwiZW1haWwiOiJubWl0QHN1cmVwYXNzLmlvIiwidGVuYW50X2lkIjoibWFpbiIsInVzZXJfY2xhaW1zIjp7InNjb3BlcyI6WyJ1c2VyIl19fQ.bknLKmz5DrUGJ3ytg4GQl_X0wa9CZwRxxV36ZRYwaIQ'
+          }
+        });
         
-        const isValidPAN = (pan) => /^[A-Z]{5}[0-9]{4}[A-Z]$/.test(pan);
-        
-        if (!name || !fuzzy || !panStatus || !isValidPAN(pan)) {
-          return res.status(400).json({
-            error: "Missing personal verification fields in the request body"
-          });
-        }
-
-        return 
-          // Return Yes or No
-          //using Data from database matching panNumber to Name
-        
-       }else {
-          // res.staus(400).json({ success: false, message: "Invalid details" });
+        console.log(response.data);
+        return res.status(201).json({ data: response.data });
+      }
+    } else {
+      return res.status(500).json({ error: "Not Valid details" });
     }
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.panverify = async (req, res) => {
+//   try {
+//     const { service, itemId, task, essentials } = req.body;
+
+//     if (!service || !itemId || task !== "verification" || !essentials) {
+//       return res
+//         .status(400)
+//         .json({ error: "Missing required fields in the request body" });
+//     }
+
+//     const itemIdVerify = jwt.verify(itemId, process.env.JWT_SECRET);
+//     if (itemIdVerify.fldID === '95601') {
+//       const { pan, fuzzy, panStatus } = essentials;
+//       console.log("true firldID")
+//       const isValidPAN = (pan) => /^[A-Z]{5}[0-9]{4}[A-Z]$/.test(pan);
+      
+//       if (!isValidPAN(pan) || !fuzzy || !panStatus ) 
+//       {
+//         console.log(pan);
+//         return res.status(400).json({
+//           error: "Missing personal verification fields in the request body"
+//         });
+//       }
+//       else{ 
+//         console.log(pan);
+
+//       const response = await axios.post('https://sandbox.surepass.io/api/v1/pan/pan', {
+//         id_number: pan 
+//       }, {
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Accept': 'application/json',
+//           'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMzk1MzQ4NiwianRpIjoiNjAwYzk1YzUtMjhkMi00OTBkLWIxOTMtNThkNGM4MzlkOGI5IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2Lm5taXRAc3VyZXBhc3MuaW8iLCJuYmYiOjE3MTM5NTM0ODYsImV4cCI6MTcxNTI0OTQ4NiwiZW1haWwiOiJubWl0QHN1cmVwYXNzLmlvIiwidGVuYW50X2lkIjoibWFpbiIsInVzZXJfY2xhaW1zIjp7InNjb3BlcyI6WyJ1c2VyIl19fQ.bknLKmz5DrUGJ3ytg4GQl_X0wa9CZwRxxV36ZRYwaIQ'
+//         }
+//       });
+      
+//       console.log(response.data);
+//       return res.status(201).json({ data: response.data });
+//     } }
+//     else {
+//       return res.status(500).json({ error: "Not Valid details" });
+//       }
+//     }
+//   catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.panverify = async (req, res) => {
+//   try {
+//     const { service, itemId, task, essentials } = req.body;
+
+//     if (!service || !itemId || task != "verification" || !essentials) {
+//       return res
+//         .status(400)
+//         .json({ error: "Missing required fields in the request body" });
+//     }
+
+//     const itemIdVerify = jwt.verify(itemId, process.env.JWT_SECRET)
+//       if(itemIdVerify.fldID ==='95601'){
+
+//         const { name, pan, fuzzy, panStatus } = essentials;
+        
+//         const isValidPAN = (pan) => /^[A-Z]{5}[0-9]{4}[A-Z]$/.test(pan);
+        
+//         if (!name || !fuzzy || !panStatus || !isValidPAN(pan)) {
+//           return res.status(400).json({
+//             error: "Missing personal verification fields in the request body"
+//           });
+
+//               const response = await axios.post('https://kyc-api.aadhaarkyc.io/api/v1/pan/pan', {
+//                 id_number: {pan}
+//               }, {
+//                 headers: {
+//                   'Content-Type': 'application/json',
+//                   'Accept': 'application/json',
+//                   'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMzk1MzQ4NiwianRpIjoiNjAwYzk1YzUtMjhkMi00OTBkLWIxOTMtNThkNGM4MzlkOGI5IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2Lm5taXRAc3VyZXBhc3MuaW8iLCJuYmYiOjE3MTM5NTM0ODYsImV4cCI6MTcxNTI0OTQ4NiwiZW1haWwiOiJubWl0QHN1cmVwYXNzLmlvIiwidGVuYW50X2lkIjoibWFpbiIsInVzZXJfY2xhaW1zIjp7InNjb3BlcyI6WyJ1c2VyIl19fQ.bknLKmz5DrUGJ3ytg4GQl_X0wa9CZwRxxV36ZRYwaIQ',
+
+//                 }
+//               });
+//               return res.status(201).json({data:response.data});
+//               console.log(response.data);
+              
+//           }
+                
+//        }else {
+//           // res.staus(400).json({ success: false, message: "Invalid details" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// };
+
+
+
+
 
 
 
---
+++ test.ts
@@ -0,0 +1,157 @@
+// featureSettings = [
+//   {
+//     featureName: "Location Details",
+//     adminVisibility: true,
+//     adminEditable: true,
+//     authorVisibility: false,
+//     authorEditable: false,
+//     subFeature: [
+//       {
+//         featureName: "Features",
+//         adminVisibility: true,
+//         adminEditable: true,
+//         authorVisibility: true,
+//         authorEditable: true,
+//         fieldName: [
+//           {
+//             name: "Feature Name",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: true,
+//           },
+//           {
+//             name: "Feature Type",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: true,
+//           },
+//           {
+//             name: "Feature Status",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: true,
+//           },
+//         ],
+//       },
+//       {
+//         featureName: "Operations",
+//         adminVisibility: true,
+//         adminEditable: true,
+//         authorVisibility: true,
+//         authorEditable: false,
+//         fieldName: [
+//           {
+//             name: "Operation Name",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Operation Type",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Operation Status",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     featureName: "Subscriptions",
+//     adminVisibility: true,
+//     adminEditable: false,
+//     authorVisibility: true,
+//     authorEditable: true,
+//     subFeature: [
+//       {
+//         featureName: "Subscription Dashboard",
+//         adminVisibility: true,
+//         adminEditable: true,
+//         authorVisibility: true,
+//         authorEditable: false,
+//         fieldName: [
+//           {
+//             name: "Subscription ID",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Subscription Status",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Subscription Date",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: true,
+//             authorEditable: false,
+//           },
+//         ],
+//       },
+//       {
+//         featureName: "Subscription Products",
+//         adminVisibility: true,
+//         adminEditable: false,
+//         authorVisibility: false,
+//         authorEditable: false,
+//         fieldName: [
+//           {
+//             name: "Product ID",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: false,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Product Name",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: false,
+//             authorEditable: false,
+//           },
+//           {
+//             name: "Product Price",
+//             adminVisibility: true,
+//             adminEditable: true,
+//             authorVisibility: false,
+//             authorEditable: false,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+// const fruits =[{ roll:"1",name:"sohan",address:"delhi"},
+// { roll:"2",name:"Mango",address:"japan"},
+// { roll:"3",name:"rohan",address:"goa"},
+// { roll:"4",name:"ram",address:"mumbai"}];
+
+//  fruits.map((fruit) => 
+//     <div key={fruit.roll} >
+//         {fruit}
+//     </div>)
+
+
+
+type Vegetable = { name: string, color: string };
+
+const vegetable: Vegetable[] = [{ name: "tomato", color: "red" },
+                              { name: "potato", color: "yellow" }];
